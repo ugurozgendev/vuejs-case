29976a5486ed035171b8b83103c5453172e79fcf
ugur <hi@ugurogen.com>
2024-09-10 12:26:04 +0300

    sayfalama eklendi (gonderiler listesi icin)

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,27 +1,59 @@
 <script setup lang="ts">
 import type { Post } from '@/types'
-import { computed } from 'vue'
-import DataTableItem from './DataTableItem.vue'
+import { computed, ref, watchEffect } from 'vue'
+import DataTableItem from '@/components/DataTableItem.vue'
+import Pagination from '@/components/Pagination.vue'
 
 const props = defineProps<{
   data: Post[]
   keywords: string
 }>()
 
+const perPage = ref(12)
+const currentPage = ref(1)
+const totalPage = ref(1)
+
+const handlePageChange = (page: number) => {
+  currentPage.value = page
+}
+
 const filteredData = computed(() => {
   const { data, keywords } = props
   const searchItem = keywords.trim().toLowerCase()
 
   if (searchItem) {
-    return data.filter((item) => item.user_name.toLowerCase().includes(searchItem))
+    return data.filter((item) => item.user.name.toLowerCase().includes(searchItem))
   }
 
   return data
+})
+
+const filteredDataPaginate = computed(() => {
+  const { data, keywords } = props
+  const searchItem = keywords.trim().toLowerCase()
+
+  // for paginate
+  const start = (currentPage.value - 1) * perPage.value
+  const end = start + perPage.value
+
+  if (searchItem) {
+    return data
+      .filter((item) => item.user.name.toLowerCase().includes(searchItem))
+      .slice(start, end)
+  }
+
+  return data.slice(start, end)
+})
+
+watchEffect(() => {
+  currentPage.value = 1
+  totalPage.value = Math.ceil(filteredData.value.length / perPage.value)
 })
 </script>
 
 <template>
   <div class="bg-[#D9D9D9] shadow-custom px-10 py-9 rounded-t-[56px] mt-8">
+    <!-- data table -->
     <table class="w-full">
       <thead>
         <tr
@@ -40,8 +72,8 @@ const filteredData = computed(() => {
         <tr class="h-14" colspan="2"></tr>
       </thead>
       <tbody class="*:bg-background has-[:checked]:*:bg-[#E3FFE4]">
-        <template v-if="filteredData.length > 0">
-          <template v-for="post in filteredData" :key="post.id">
+        <template v-if="filteredDataPaginate.length > 0">
+          <template v-for="post in filteredDataPaginate" :key="post.id">
             <DataTableItem :post="post" />
           </template>
         </template>
@@ -52,5 +84,8 @@ const filteredData = computed(() => {
         </template>
       </tbody>
     </table>
+
+    <!-- pagination -->
+    <Pagination :totalPage="totalPage" :currentPage="currentPage" @update:page="handlePageChange" />
   </div>
 </template>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ArrowLeftCircle, ArrowRightCircle } from 'lucide-vue-next'
+
+const { totalPage, currentPage } = defineProps<{
+  totalPage: number
+  currentPage: number
+}>()
+
+const goToPage = (page: number) => {
+  if (page > 0 && page <= totalPage) {
+    emit('update:page', page)
+  }
+}
+
+const emit = defineEmits<{
+  (e: 'update:page', page: number): void
+}>()
+</script>
+
+<template>
+  <div class="text-end" v-if="totalPage > 1">
+    <div class="inline-flex items-center bg-background gap-4 rounded-full p-4 ms-auto">
+      <button @click="goToPage(currentPage - 1)" :disabled="currentPage === 1">
+        <ArrowLeftCircle />
+      </button>
+      <div class="flex *:block *:px-3.5 *:py-0.5 *:rounded-lg *:border *:border-transparent">
+        <button
+          v-for="page in totalPage"
+          :key="page"
+          @click="goToPage(page)"
+          :class="{ ' !border-[#8B8C8D]': page === currentPage }"
+        >
+          {{ page }}
+        </button>
+      </div>
+      <button @click="goToPage(currentPage + 1)" :disabled="currentPage === totalPage">
+        <ArrowRightCircle />
+      </button>
+    </div>
+  </div>
+</template>
